ef976c4c2fc52edd224964d2c851eca89dda2746
shirok420 <50665131+shirok420@users.noreply.github.com>
2025-04-15 11:57:08 +0900

    fix: ESLint設定の追加とhusky設定の修正

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // Next.jsではReactのimportが不要
+      "react/react-in-jsx-scope": "off",
+      // anyの使用を一時的に許可
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  }
+]);
